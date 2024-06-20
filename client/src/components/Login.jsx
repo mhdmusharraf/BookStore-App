d6f6dfc2a88a51bc556b06b397e694134a249076
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-06-20 22:19:15 +0530

    Until login ui completed

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "../css/Login.css";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("student");
+
+  const handleSubmit = () => {
+    
+  }
+ 
+
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <h2>Login</h2> <br />
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="role">Role:</label>
+          <select
+            name="role"
+            id="role"
+            onChange={(e) => setRole(e.target.value)}
+          >
+            <option value="admin">Admin</option>
+            <option value="user">Student</option>
+          </select>
+        </div>
+        <button className="btn-login" onClick={handleSubmit}>Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
